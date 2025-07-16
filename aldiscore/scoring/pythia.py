660af0db1cde756fa20d93cf7bc411ccaf418e17
Mattis Bodynek <mattis.bodynek@h-its.org>
2025-07-16 20:04:22 +0200

    improved pythia integration

--- aldiscore/scoring/pythia.py
+++ aldiscore/scoring/pythia.py
@@ -22,6 +22,7 @@ def compute_pythia_difficulty(ensemble: Ensemble, raxml_path: Path, threads: int
         import pypythia.prediction
         import pypythia.raxmlng
         import pypythia.custom_types
+        import pypythia.custom_errors
         import pypythia.msa
     except ImportError:
         raise ImportError(
@@ -39,10 +40,11 @@ def compute_pythia_difficulty(ensemble: Ensemble, raxml_path: Path, threads: int
             file = Path(tmpfile.name)
             AlignIO.write([alignment.msa], file, "fasta")
             msa = pypythia.msa.parse_msa(file, pypythia.custom_types.FileFormat.FASTA)
-
-            feats = pypythia.prediction.collect_features(
-                msa=msa, msa_file=file, raxmlng=RAXMLNG, threads=threads
-            )
-            scores.append(PYTHIA.predict(feats)[0])
-
+            try:
+                feats = pypythia.prediction.collect_features(
+                    msa=msa, msa_file=file, raxmlng=RAXMLNG, threads=threads
+                )
+                scores.append(PYTHIA.predict(feats)[0])
+            except pypythia.custom_errors.RAxMLNGError:
+                scores.append(np.nan)
     return np.array(scores)
